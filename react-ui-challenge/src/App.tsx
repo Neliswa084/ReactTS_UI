3edfe83ac101c8aaba0dbfc277a50e475624f1e6
Neliswa084 <neliswangcobo141@gmail.com>
2026-07-06 15:23:56 +0200

    created hero component and f=move it from App.tsx to Hero.tsx i created

--- react-ui-challenge/src/App.tsx
+++ react-ui-challenge/src/App.tsx
@@ -1,8 +1,7 @@
 import  './App.css'
 import { Text } from './components/Text/Text'
 import { Navbar } from './components/Navbar/Navbar'
-import fruit from './assets/FruitImage.png'
-import mugorange from'./assets/Mug_and_Orange.png'
+import {Hero} from './components/Hero/Hero'
 
 function App() {
 
@@ -11,26 +10,8 @@ function App() {
      <>
      
      <Navbar />
+     <Hero />
 
-    <div className="hero">
-      <div className="hero-text">
-        <Text variant={'h2'}>Premium Restaurant  </Text>
-           {/* <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> SIGN UP</button> */}
-      <Text variant='h1' >Anida Dedelay</Text>
-      <Text variant={'h2'}> BEST HEALTHY SALAD SERVED </Text>
-      <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> LEARN MORE</button>
-    
-     </div>
-  
-     <img src={fruit} alt="fruit" style={{alignContent:'center'}}/>
-     </div>
-     <div className="section">
-      <div className="container">
-        <Text variant={'h1'} >Orange Benefit  </Text>
-         <img src={mugorange} alt="OrangeMug" /> 
-            <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> LEARN MORE</button>
-        </div>
-     </div>
     </>
   )
 }
---
+++ react-ui-challenge/src/components/Hero/Hero.tsx
@@ -0,0 +1,31 @@
+import './Hero.module.css'
+import { Text } from '../Text/Text'
+import mugorange from '../../assets/Mug_and_Orange.png'
+
+export const Hero = () => {
+  return (
+    <div>
+
+
+     <div className="hero">
+      <div className="hero-text">
+        <Text variant={'h2'}>Premium Restaurant  </Text>
+           {/* <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> SIGN UP</button> */}
+      <Text variant='h1' >Anida Dedelay</Text>
+      <Text variant={'h2'}> BEST HEALTHY SALAD SERVED </Text>
+      <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> LEARN MORE</button>
+    
+     </div>
+  
+      
+     </div>
+     <div className="section">
+      <div className="container">
+        <Text variant={'h1'} >Orange Benefit  </Text>
+         <img src={mugorange} alt="OrangeMug" /> 
+            <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> LEARN MORE</button>
+        </div>
+     </div>
+    </div>
+  )
+}
