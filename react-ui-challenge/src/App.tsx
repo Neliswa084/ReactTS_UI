6f2e46aa5d55d6b65a3affba0d9e230b3d7f3bfb
Neliswa084 <neliswangcobo141@gmail.com>
2026-07-06 13:31:29 +0200

    started the second section

--- react-ui-challenge/src/App.tsx
+++ react-ui-challenge/src/App.tsx
@@ -2,6 +2,7 @@ import  './App.css'
 import { Text } from './components/Text/Text'
 import { Navbar } from './components/Navbar/Navbar'
 import fruit from './assets/FruitImage.png'
+import mugorange from'./assets/Mug_and_Orange.png'
 
 
 
@@ -16,16 +17,22 @@ function App() {
      <Navbar />
 
     <div className="hero">
-      
       <div className="hero-text">
+        <Text variant={'h2'}>Premium Restaurant  </Text>
+           {/* <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> SIGN UP</button> */}
       <Text variant='h1' >Anida Dedelay</Text>
       <Text variant={'h2'}> BEST HEALTHY SALAD SERVED </Text>
-      <button style={{color:'white' , backgroundColor:'orange', height:'60px',width:'90px'}}> Learn More</button>
+      <button style={{color:'white' , backgroundColor:'orange', height:'50px',width:'120px'}}> LEARN MORE</button>
     
      </div>
-       <img src={fruit} alt="fruit" />
+  
+     <img src={fruit} alt="fruit" style={{alignContent:'center'}}/>
      </div>
-
+     <div className="hero">
+        <Text variant={'h1'}>Orange Benefit  </Text>
+        <img src={mugorange} alt="OrangeMug"/>
+        
+     </div>
     </>
   )
 }
--- react-ui-challenge/src/components/Navbar/Navbar.tsx
+++ react-ui-challenge/src/components/Navbar/Navbar.tsx
@@ -3,13 +3,17 @@ import {Text} from '../Text/Text'
 import styles from './Navbar.module.css'
 import { ContentContainer } from '../ContentContainer'
 
+
 export const Navbar = () => {
   return (
    
     <nav>
+      
          <div className={styles.content}>
-            <Text variant={'span'}style={{margin:0,color:'white'}} >Premium Restaurant</Text>
-              <div className={styles.links}>
+
+            <Text variant={'span'}style={{margin:0,color:'white'}} >Fruit Restaurant</Text>
+            
+       <div className={styles.links}>
         <a href='/#' className={styles.link}> HOME</a> 
         <a href='/#' className={styles.link}> MENU</a>
         <a href='/#' className={styles.link}> PAGES</a>
@@ -17,12 +21,12 @@ export const Navbar = () => {
         <a href='/#' className={styles.link}> CONTACT US</a>
 
          <div className={styles ['profile-icon']}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white">
   <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
 </svg>
 </div>
   <div className={styles ['profile-icon']}>
-           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" viewBox="0 0 16 16">
+           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" viewBox="0 0 16 16">
        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
         </svg>
 
