661cd1c7038573f129c5a34c72b12d6e54e0d9c7
Neliswa084 <neliswangcobo141@gmail.com>
2026-07-07 16:15:25 +0200

    Cards section

--- react-ui-challenge/src/App.tsx
+++ react-ui-challenge/src/App.tsx
@@ -3,16 +3,41 @@ import { Text } from './components/Text/Text'
 import { Navbar } from './components/Navbar/Navbar'
 import {Hero} from './components/Hero/Hero'
 import { OrangeBenefit } from './components/OrangeBenefit/OrangeBenefit'
+import { Cards } from './components/CardsImage/Cards'
+
+import OrangeOne from './assets/Orange_One.png'
+import OrangeTwo from './assets/Orange_two.png'
+import FruitImage from './assets/picture3.png'
+import { useState } from 'react'
+
+
 
 function App() {
 
-
+    // const [menu] = useState ([
+    //      {name: "ARCU VOLUT FAT VITAE", image:OrangeOne, description: "Phestiers 1geart"},
+    //      {name: "$19,99,90,99E", image:OrangeTwo, description: "Orviste past brorps"},
+    //      {name: "SFIELY TUN DRENARD", image:FruitImage, description: "Ongand suniers"}
+    // ])
   return (
      <>
      
      <Navbar />
      <Hero />
      <OrangeBenefit />
+     <Cards />
+
+     {/* {
+      menu && menu.length> 0 && menu.map(product =>
+      {
+        return <Cards 
+        
+        name={product.name}
+        description={product.description}
+        image={product.image}
+      />
+      })
+     } */}
 
     </>
   )
---
+++ react-ui-challenge/src/components/CardsImage/Cards.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Orange_One from '../../assets/Orange_One.png'
+import OrangeTwo from '../../assets/Orange_two.png'
+import Fruit from '../../assets/image.png'
+
+export const Cards = () => {
+  return (
+    <div>
+
+
+
+    </div>
+  )
+}
