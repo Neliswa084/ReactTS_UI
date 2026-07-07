490bb8ec24bd4f40e66446971ec85ce998502587
Neliswa Ngcobo <Neliswa Ngcobo@DESKTOP-2TH32QS>
2026-07-07 21:25:50 +0200

    Fixed Card section

--- react-ui-challenge/src/App.tsx
+++ react-ui-challenge/src/App.tsx
@@ -1,43 +1,42 @@
 import  './App.css'
-import { Text } from './components/Text/Text'
 import { Navbar } from './components/Navbar/Navbar'
 import {Hero} from './components/Hero/Hero'
 import { OrangeBenefit } from './components/OrangeBenefit/OrangeBenefit'
+import { useState } from 'react'
 import { Cards } from './components/CardsImage/Cards'
 
 import OrangeOne from './assets/Orange_One.png'
 import OrangeTwo from './assets/Orange_two.png'
-import FruitImage from './assets/picture3.png'
-import { useState } from 'react'
-
-
+import Fruits from './assets/picture3.png'
 
 function App() {
 
-    // const [menu] = useState ([
-    //      {name: "ARCU VOLUT FAT VITAE", image:OrangeOne, description: "Phestiers 1geart"},
-    //      {name: "$19,99,90,99E", image:OrangeTwo, description: "Orviste past brorps"},
-    //      {name: "SFIELY TUN DRENARD", image:FruitImage, description: "Ongand suniers"}
-    // ])
+
+  const [menu ] = useState([
+    {name: "ARCU VOLUT FAT VITAE" , image: OrangeOne, description:"Phestiers 1geart"},
+    {name: "$19,99,90,99E" , image: OrangeTwo, description:"Orviste past brops"},
+    {name: "SFIELY TUN DENARD" , image: Fruits, description:"Ongand suniers"}
+  ])
+
   return (
      <>
      
      <Navbar />
      <Hero />
      <OrangeBenefit />
-     <Cards />
-
-     {/* {
+     <div className="cards-container">
+        {
+      
       menu && menu.length> 0 && menu.map(product =>
       {
-        return <Cards 
-        
-        name={product.name}
+        return <Cards name={product.name} 
         description={product.description}
-        image={product.image}
-      />
+        image={product.image} />
       })
-     } */}
+     }
+     </div>
+   
+     
 
     </>
   )
--- react-ui-challenge/src/components/CardsImage/Cards.tsx
+++ react-ui-challenge/src/components/CardsImage/Cards.tsx
@@ -2,12 +2,21 @@ import React from 'react'
 import Orange_One from '../../assets/Orange_One.png'
 import OrangeTwo from '../../assets/Orange_two.png'
 import Fruit from '../../assets/image.png'
+import styles from './Cards.module.css'
+import { Text } from '../Text/Text'
 
-export const Cards = () => {
+type CardProps={
+  name: string,
+  description: string,
+  image: string
+}
+
+export const Cards: React.FC<CardProps> = ({name, description, image}) => { 
   return (
-    <div>
-
-
+    <div className={styles['item-card']}> 
+   <img className={styles['item-image']} src={image} alt={'image product'}/>
+   <Text variant={'h2'} >{name}</Text>
+   <Text variant={'p'}>{description}</Text>
 
     </div>
   )
